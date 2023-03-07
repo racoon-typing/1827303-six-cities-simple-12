2123528479889f09ed13701db87bd1eab1d293ce
Daniil <eo18622@yandex.ru>
2023-03-07 15:51:44 +0300

    Добавляет путь на главную для ссылок

--- project/src/components/cities-card/cities-card.tsx
+++ project/src/components/cities-card/cities-card.tsx
@@ -5,7 +5,7 @@ function CitiesCard(): JSX.Element {
         <span>Premium</span>
       </div>
       <div className="cities__image-wrapper place-card__image-wrapper">
-        <a href="#">
+        <a href="/">
           <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place apartment" />
         </a>
       </div>
@@ -24,7 +24,7 @@ function CitiesCard(): JSX.Element {
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+          <a href="/">Beautiful &amp; luxurious apartment at great location</a>
         </h2>
         <p className="place-card__type">Apartment</p>
       </div>
