219ffba7508b076b5b1df6c10c03d26333ee801b
Daniil <eo18622@yandex.ru>
2023-03-09 09:09:05 +0300

    Добавляет HelmetProvider для семантики

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {AppRoute} from '../../consts';
+import { HelmetProvider } from 'react-helmet-async';
+import { AppRoute } from '../../consts';
 
 import Main from '../../pages/main/main';
 import Login from '../../pages/login/login';
@@ -13,19 +14,21 @@ type AppScreenProps = {
 
 function App({ numOfFlat }: AppScreenProps): JSX.Element {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={AppRoute.Root} element={<Layuot />}>
-          <Route index element={<Main numOfFlat={numOfFlat} />} />
-          <Route path={AppRoute.Login} element={<Login />} />
-          <Route path={AppRoute.Offer}>
-            <Route index element={<Room />} />
-            <Route path=':id' element={<Room />} />
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={AppRoute.Root} element={<Layuot />}>
+            <Route index element={<Main numOfFlat={numOfFlat} />} />
+            <Route path={AppRoute.Login} element={<Login />} />
+            <Route path={AppRoute.Offer}>
+              <Route index element={<Room />} />
+              <Route path=':id' element={<Room />} />
+            </Route>
           </Route>
-        </Route>
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
--- project/src/pages/not-found/not-found.tsx
+++ project/src/pages/not-found/not-found.tsx
@@ -1,21 +1,29 @@
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+
 import './style.css';
 
 function NotFound(): JSX.Element {
   return (
-    <section className='not-found'>
-      <div className='not-found__content'>
-        <h1 className='not-found__title'>404</h1>
+    <>
+      <Helmet>
+        <title>Страница не найдена</title>
+      </Helmet>
 
-        <p className="not-found__text">
-          Not Found
-        </p>
+      <section className='not-found'>
+        <div className='not-found__content'>
+          <h1 className='not-found__title'>404</h1>
 
-        <Link className='not-found__link' to="/">
-          На главную
-        </Link>
-      </div>
-    </section>
+          <p className="not-found__text">
+            Not Found
+          </p>
+
+          <Link className='not-found__link' to="/">
+            На главную
+          </Link>
+        </div>
+      </section>
+    </>
   );
 }
 
