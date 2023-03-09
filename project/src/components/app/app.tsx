d890f87f942ae3c6c2e5fff8aa5ad6cc2a17ecc7
Daniil <eo18622@yandex.ru>
2023-03-09 08:30:16 +0300

    Добавляет Layuot и файл с путями

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {AppRoute} from '../../consts';
 
 import Main from '../../pages/main/main';
 import Login from '../../pages/login/login';
 import Room from '../../pages/room/room';
 import NotFound from '../../pages/not-found/not-found';
+import Layuot from '../../components/layuot/layuot';
 
 type AppScreenProps = {
   numOfFlat: number;
@@ -13,10 +15,10 @@ function App({ numOfFlat }: AppScreenProps): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/'>
+        <Route path={AppRoute.Root} element={<Layuot />}>
           <Route index element={<Main numOfFlat={numOfFlat} />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='offer/'>
+          <Route path={AppRoute.Login} element={<Login />} />
+          <Route path={AppRoute.Offer}>
             <Route index element={<Room />} />
             <Route path=':id' element={<Room />} />
           </Route>
