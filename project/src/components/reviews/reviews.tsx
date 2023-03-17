414e51531044ed59cbd7ddd20f1a3765f10a62ae
Daniil <eo18622@yandex.ru>
2023-03-17 09:33:44 +0300

    Работа формы

--- project/src/components/reviews/reviews.tsx
+++ project/src/components/reviews/reviews.tsx
@@ -1,7 +1,7 @@
-import Rating from '../rating/rating';
+// import Rating from '../rating/rating';
 import { useState } from 'react';
 
-type FieldChangeHandle = React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>;
+type FieldChangeHandle = React.FormEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>;
 
 function Reviews() {
   const [formData, setFormData] = useState({
@@ -9,9 +9,9 @@ function Reviews() {
     review: ''
   });
 
-  const fieldChangeHandle = (evt: FieldChangeHandle): void => {
-    const { name, value } = evt.target;
-    setFormData({ ...formData, [name]: value });
+  const fieldChangeHandler = (evt: FieldChangeHandle): void => {
+    const {name, value} = evt.target as HTMLInputElement;
+    setFormData({...formData, [name]: value});
   };
 
   return (
@@ -44,43 +44,48 @@ function Reviews() {
       <form className="reviews__form form" action="#" method="post">
         <label className="reviews__label form__label" htmlFor="review">Your review</label>
         <div className="reviews__rating-form form__rating">
-          <Rating onChangeHandler={fieldChangeHandle} />
-          {/* <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" onChange={fieldChangeHandle} />
+          <input className="form__rating-input visually-hidden" name="rating" value='5' id='5-stars' type="radio"
+            onClick={fieldChangeHandler}
+          />
           <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
             </svg>
-          </label> */}
-
-          <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" onChange={fieldChangeHandle} />
-          <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
+          </label>
+          <input className="form__rating-input visually-hidden" name="rating" value='4' id='4-stars' type="radio"
+            onClick={fieldChangeHandler}
+          />
+          <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
             </svg>
           </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" onChange={fieldChangeHandle} />
-          <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
+          <input className="form__rating-input visually-hidden" name="rating" value='3' id='3-stars' type="radio"
+            onClick={fieldChangeHandler}
+          />
+          <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
             </svg>
           </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" onChange={fieldChangeHandle} />
-          <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
+          <input className="form__rating-input visually-hidden" name="rating" value='2' id='2-stars' type="radio"
+            onClick={fieldChangeHandler}
+          />
+          <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
             </svg>
           </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" onChange={fieldChangeHandle} />
-          <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
+          <input className="form__rating-input visually-hidden" name="rating" value='1' id='1-stars' type="radio"
+            onClick={fieldChangeHandler}
+          />
+          <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
             </svg>
           </label>
         </div>
-        <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" onChange={fieldChangeHandle}></textarea>
+        <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" onChange={fieldChangeHandler}></textarea>
         <div className="reviews__button-wrapper">
           <p className="reviews__help">
             To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
