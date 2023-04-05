60b7a30c75067dbfc6f5b6bb5e5733872c3808c1
Daniil <eo18622@yandex.ru>
2023-04-05 09:14:59 +0300

    ghj,tk

---
+++ project/src/store/api-actions.ts
@@ -0,0 +1,50 @@
+import {AxiosInstance} from 'axios';
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import {AppDispatch, State} from '../types/state';
+import { ConstructorRoom } from '../types/offer';
+import { setLoadOffersStatus, loadOffers, requireAuthorization, setError } from './action';
+import { APIRoute, AppRoute, AuthorizationStatus, TIMEOUT_SHOW_ERROR } from '../consts/consts';
+import { store } from '.';
+
+
+export const clearErrorAction = createAsyncThunk(
+  'main/clearError',
+  () => {
+    setTimeout(
+      () => store.dispatch(setError(null)),
+      TIMEOUT_SHOW_ERROR
+    );
+  },
+);
+
+
+
+export const fetchOffersAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'data/loadOffers',
+  async (_arg, {dispatch, extra: api}) => {
+    dispatch(setLoadOffersStatus(true));
+    const {data} = await api.get<ConstructorRoom[]>(APIRoute.Offers);
+    dispatch(setLoadOffersStatus(false));
+    dispatch(loadOffers(data));
+  },
+);
+
+export const checkAuthAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/checkAuth',
+  async (_arg, {dispatch, extra: api}) => {
+    try {
+      await api.get(AppRoute.Login);
+      dispatch(requireAuthorization(AuthorizationStatus.Auth));
+    } catch {
+      dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
+    }
+  }
+);
