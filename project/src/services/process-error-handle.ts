697f9c784a54676b144076cea0413e9adfc924cc
Daniil <eo18622@yandex.ru>
2023-04-13 16:55:53 +0300

    не удалил ошибку

--- project/src/services/process-error-handle.ts
+++ project/src/services/process-error-handle.ts
@@ -1,9 +1,10 @@
+// import { NameSpace } from '../consts/consts';
 import { store } from '../store';
 // import { setError } from '../store/action';
 import { clearErrorAction } from '../store/api-actions';
 import { setError } from '../store/data-process/data-process';
 
-// export const processErrorHandle = (message: string):void => {
-//   store.dispatch(setError(message));
-//   store.dispatch(clearErrorAction());
-// };
+export const processErrorHandle = (message: string):void => {
+  store.dispatch(setError(message));
+  store.dispatch(clearErrorAction());
+};
