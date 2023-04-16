76d9bc55b75b041c179de63f821590b71bcdcccd
Daniil <eo18622@yandex.ru>
2023-04-16 13:30:41 +0300

    пустая страница

--- project/src/pages/main/main.tsx
+++ project/src/pages/main/main.tsx
@@ -69,6 +69,18 @@ function Main(): JSX.Element {
         ) : (
           <LoadingScreen />
         )}
+
+{/* <div class="cities">
+          <div class="cities__places-container cities__places-container--empty container">
+            <section class="cities__no-places">
+              <div class="cities__status-wrapper tabs__content">
+                <b class="cities__status">No places to stay available</b>
+                <p class="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
+              </div>
+            </section>
+            <div class="cities__right-section"></div>
+          </div>
+        </div> */}
       </main>
     </>
   );
