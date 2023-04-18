d7cec3f9839f57aed631a6be6675a9a4bd4ce129
Daniil <eo18622@yandex.ru>
2023-04-18 09:25:37 +0300

    дата

--- project/src/components/review-list/review-list.tsx
+++ project/src/components/review-list/review-list.tsx
@@ -12,6 +12,16 @@ function ReviewList({ reviews }: ReviewListProps) {
     maxReview = reviews.slice(0, 10);
   }
 
+  // console.log(reviews);
+  // console.log(reviews[0].date);
+  // console.log(typeof reviews[0].date);
+  // console.log(new Date(reviews[0].date));
+
+  // const myDate = new Date(reviews[0].date);
+  // console.log(myDate.parse);
+
+
+  // sort((a, b) => a.date - b.date)
   return (
     <ul className="reviews__list">
       {maxReview.map((review, id) => (
