e99612aef9f4967b9c1746c383e7dd8c64e6038d
Daniil <eo18622@yandex.ru>
2023-04-19 11:56:07 +0300

    редьюсер для мейн-процесс

--- project/src/store/main-process/main-process.test.ts
+++ project/src/store/main-process/main-process.test.ts
@@ -1,10 +1,21 @@
 import { mainProcess } from './main-process';
 import {changeCity, hoverCity} from './main-process';
 
-
 describe('Reducer: mainProcess', () => {
   it('without additional parameters should return initial state', () => {
-    expect(mainProcess.reducer(void 0, {type: 'UNKNOWN_ACTION'}))
-      .toEqual({city: 'Paris', hoverCity: 0});
-  }
+    expect(mainProcess.reducer(void 0, { type: 'UNKNOWN_ACTION' }))
+      .toEqual({ city: 'Paris', hoverCity: 0 });
+  });
+
+  it('should return city value', () => {
+    const state = {city: 'Paris', hoverCity: 0};
+    expect(mainProcess.reducer(state, changeCity({activeCity: 'Amsterdam'})))
+      .toEqual({ city: 'Amsterdam', hoverCity: 0});
+  });
+
+  it('should return Id hovered city', () => {
+    const state = {city: 'Paris', hoverCity: 0};
+    expect(mainProcess.reducer(state, hoverCity({hoveredCity: 5})))
+      .toEqual({city: 'Paris', hoverCity: 5});
+  });
 });
